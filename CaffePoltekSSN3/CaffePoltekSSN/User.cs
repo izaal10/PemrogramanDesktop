355264c5c600f72f9ea4376ab643080560c647d7
izaal10 <99273053+izaal10@users.noreply.github.com>
2023-05-28 15:08:17 +0700

    Add files via upload

---
+++ CaffePoltekSSN3/CaffePoltekSSN/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaffePoltekSSN
+{
+    internal class User
+    {
+        //class properties
+        private string _username = string.Empty;
+        private string _password = string.Empty;
+        public string _rule = string.Empty;
+
+        //class constructor
+        public User(string username, string password, string rule)
+        {
+            this._username = username;
+            this._password = password;
+            this._rule = rule;
+        }
+
+        //method getter setter
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public string Rule
+        {
+            get { return _rule; }
+            set { _rule = value; }
+        }
+
+    }
+}
